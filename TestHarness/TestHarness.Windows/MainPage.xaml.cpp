cf260cb2c35838ed9cdbcad400e30f5839ae4dce
U-REDMOND\v-oniobi <v-oniobi@v-oniobi-1217.redmond.corp.microsoft.com>
2015-02-18 06:04:29 -0800

    Change when_all to when_any and successively replaced C++/CX types with C++ types = more perf

--- TestHarness/TestHarness.Windows/MainPage.xaml.cpp
+++ TestHarness/TestHarness.Windows/MainPage.xaml.cpp
@@ -27,13 +27,16 @@ void TestHarness::MainPage::testButton_Click(Platform::Object^ sender, Windows::
 	auto _listOfRoundTripTimes = ref new Vector<RoundTripDataPoint^>();
 	listOfRoundTripTimes->ItemsSource = _listOfRoundTripTimes;
 
-	//set test domains and test array of sockets
-	auto serverHostNames = ref new Array<String^>{ "google.com", "bing.com", "facebook.com", "yahoo.com" };
-	auto streamSocket = ref new Array<StreamSocket^>(__numberOfTrials * serverHostNames->Length);
+	//Set test domains and test array of sockets
+	auto serverHostNames = new array<String^, 4> { {"google.com", "bing.com", "facebook.com", "yahoo.com" } };
+	auto streamSocket = ref new Array<StreamSocket^>(__numberOfTrials * serverHostNames->size());
+
+	//Use a decrementable number for the trial count
+	auto trialCount = __numberOfTrials * serverHostNames->size();
 
 	//set time duration for task cancels
 	TimeSpan delay;
-	delay.Duration = 1400000;
+	delay.Duration = 400000;
 
 	//Prepare cancellation token information for tasks
 	map<unsigned int,cancellation_token_source*> cts;
@@ -41,35 +44,36 @@ void TestHarness::MainPage::testButton_Click(Platform::Object^ sender, Windows::
 
 	//create trial tasks
 	vector<task<void>> connectTask;
-	for (unsigned int trialIndex = 0; trialIndex < __numberOfTrials * serverHostNames->Length; trialIndex++)
+	for (unsigned int trialIndex = 0; trialIndex < trialCount; trialIndex++)
 	{
 		streamSocket[trialIndex] = ref new StreamSocket();
 		cts.insert(pair<unsigned int, cancellation_token_source*>(trialIndex, new cancellation_token_source()));
-		connectTask.push_back(create_task(streamSocket[trialIndex]->ConnectAsync(ref new HostName(serverHostNames[(trialIndex + 1) % serverHostNames->Length]), ref new String(L"80"), SocketProtectionLevel::PlainSocket), cts[trialIndex]->get_token())
-			.then([cts, trialIndex, delay, delayTimer]() mutable
+		connectTask.push_back(create_task(streamSocket[trialIndex]->ConnectAsync(ref new HostName(serverHostNames->at((trialIndex + 1) % serverHostNames->size())), ref new String(L"80"), SocketProtectionLevel::PlainSocket), cts[trialIndex]->get_token())
+			.then([cts, trialCount, trialIndex, delay, delayTimer]() mutable
 		{
 			delayTimer.push_back(ThreadPoolTimer::CreateTimer(ref new TimerElapsedHandler([cts, trialIndex](ThreadPoolTimer^ threadPoolTimer)
 			{
 				cts.at(trialIndex)->cancel();
 			}), delay));
-		}, task_continuation_context::use_current()));
+		}, task_continuation_context::use_current()).then([trialCount](task<void> previousTask) mutable
+		{
+			try { previousTask.get(); }
+			catch (COMException^){ trialCount--; }
+			catch (task_canceled&){ trialCount--; }
+		}));
 	}
 
 	//execute ping tasks, then calculate the mean and report results
-	when_all(begin(connectTask), end(connectTask))
-		.then([streamSocket, _listOfRoundTripTimes, __numberOfTrials, serverHostNames, this]() mutable 
+	when_any(begin(connectTask), end(connectTask))
+		.then([streamSocket, _listOfRoundTripTimes, trialCount, this](size_t result) mutable 
 	{
 		float32 speedTotal = 0.0;
-		for (unsigned int trialIndex = 0; trialIndex < __numberOfTrials * serverHostNames->Length; trialIndex++)
+		for (unsigned int trialIndex = 0; trialIndex < trialCount; trialIndex++)
 		{
 			speedTotal += streamSocket[trialIndex]->Information->RoundTripTimeStatistics.Min / 1000000.0F;
+			if (streamSocket[trialIndex]->Information->RoundTripTimeStatistics.Min == 0.000000F) trialCount--;
 			_listOfRoundTripTimes->Append(ref new RoundTripDataPoint(streamSocket[trialIndex]->Information->RoundTripTimeStatistics.Min / 1000000.0F));
 		}
-		meanRoundTripTime->Text = (speedTotal / (float32)(__numberOfTrials * serverHostNames->Length)).ToString();
-	}).then([](task<void> previousTask)
-	{
-		try { previousTask.get(); }
-		catch (COMException^){}
-		catch (task_canceled&){}
+		meanRoundTripTime->Text = (speedTotal / (float32)(trialCount)).ToString();
 	});
 }
